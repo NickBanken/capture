a5586dfab42cffc429997aa1ce4efb420cc43b74
Nick <nick.banken@gmail.com>
2021-03-11 14:02:18 +0100

    set up FAQ section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="awnser">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum
+            temporibus dicta reiciendis hic nisi libero facere nam recusandae
+            ducimus, eaque eum incidunt eos aperiam veniam amet architecto. Ut,
+            debitis qui. Quia deleniti facere provident in adipisci nulla velit
+            numquam, voluptates atque impedit officia voluptatem, est illum
+            totam repellat quidem nihil! Aliquam iusto doloribus autem,
+            explicabo velit deleniti ipsam praesentium nesciunt!
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="awnser">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum
+            temporibus dicta reiciendis hic nisi libero facere nam recusandae
+            ducimus, eaque eum incidunt eos aperiam veniam amet architecto. Ut,
+            debitis qui. Quia deleniti facere provident in adipisci nulla velit
+            numquam, voluptates atque impedit officia voluptatem, est illum
+            totam repellat quidem nihil! Aliquam iusto doloribus autem,
+            explicabo velit deleniti ipsam praesentium nesciunt!
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <div className="awnser">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum
+            temporibus dicta reiciendis hic nisi libero facere nam recusandae
+            ducimus, eaque eum incidunt eos aperiam veniam amet architecto. Ut,
+            debitis qui. Quia deleniti facere provident in adipisci nulla velit
+            numquam, voluptates atque impedit officia voluptatem, est illum
+            totam repellat quidem nihil! Aliquam iusto doloribus autem,
+            explicabo velit deleniti ipsam praesentium nesciunt!
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>What Products Do You Offer?</h4>
+        <div className="awnser">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum
+            temporibus dicta reiciendis hic nisi libero facere nam recusandae
+            ducimus, eaque eum incidunt eos aperiam veniam amet architecto. Ut,
+            debitis qui. Quia deleniti facere provident in adipisci nulla velit
+            numquam, voluptates atque impedit officia voluptatem, est illum
+            totam repellat quidem nihil! Aliquam iusto doloribus autem,
+            explicabo velit deleniti ipsam praesentium nesciunt!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
