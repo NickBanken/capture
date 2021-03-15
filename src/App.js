d1194deacaa232b55d25ee2f8880a5b2a567ac58
Nick <nick.banken@gmail.com>
2021-03-15 20:14:15 +0100

    created movies section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import AboutSection from "./components/AboutSection";
 //Global Style
 import GlobalStyle from "./components/GlobalStyle";
 //import pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import Nav from "./components/Nav";
+import MovieDetail from "./pages/MovieDetail";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" component={AboutUs} exact></Route>
+        <Route path="/work" component={OurWork} exact></Route>
+        <Route path="/work/:id">
+          <MovieDetail></MovieDetail>
+        </Route>
+        <Route path="/contact" component={ContactUs}></Route>
+      </Switch>
     </div>
   );
 }
