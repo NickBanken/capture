582413d22b65d46edcf367825dbb72a6279ae4f5
Nick <nick.banken@gmail.com>
2021-03-18 11:13:22 +0100

    addition of page animation

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,14 +2,22 @@ import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServiceSection from "../components/ServicesSection";
 import FaqSection from "../components/FaqSection";
+//animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
 
 const AboutUs = () => {
   return (
-    <>
+    <motion.div
+      exit="exit"
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+    >
       <AboutSection />
       <ServiceSection />
       <FaqSection />
-    </>
+    </motion.div>
   );
 };
 
